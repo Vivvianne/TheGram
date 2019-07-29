83157f2d187fbfc9b55c1b7606a9664775f60aa9
Vivvianne Kimani <ndiahav@gmail.com>
2019-07-29 09:32:49 +0300

    added tests

--- thegram/tests.py
+++ thegram/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from . models import Post
 
 # Create your tests here.
+
+class PostTestClass(TestCase):
+    def setUp(self):
+        self.test_post = image(images=list('Beautiful butterflies'))
+        self.test_post.save_post()
+
+        self.image = Post(id=1,description="Slide Away")
+        self.image.save_image()
+
+    def tearDown(self):
+       Post.objects.all().delete()
+      
--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+class ProfileTestClass(TestCase):
+    def setUp(self):
+        self.test_profile = profile(images=list('Beautiful butterflies'))
+        self.test_profile.save_profile()
+
+       
+
+    def tearDown(self):
+       profile.objects.all().delete()
